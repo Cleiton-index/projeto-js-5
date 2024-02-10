95deef0816105907ef661e69e4f4397bba0e6e6f
Cleiton <cleitonarguelhodasilva2@gmail.com>
2024-02-10 10:33:26 -0400

    projeto.JavaScript.01

---
+++ javascrypt.js
@@ -0,0 +1,63 @@
+const list = document.querySelector('ul')
+const buttonShowAll = document.querySelector('.show-all')
+const buttonMapAll = document.querySelector('.map-all')
+const sumAll = document.querySelector('.sum-all')
+const filterAll = document.querySelector('.filter-all')
+
+
+function formatCurrency(value){
+    const newValue = value.toLocaleString('pt-BR', { style: 'currency',
+     currency : 'BRL' })
+     return newValue
+}
+
+
+function showAll(productsArray) {
+    let myLi = ''
+    productsArray.forEach((product) => {
+        myLi += `
+              <li>
+                   <img src=${product.src}
+                   <p>${product.name}</p>
+                   <p class="item-price"> ${formatCurrency(product.price)}</p>
+             </li>
+ 
+            `
+    })
+    list.innerHTML = myLi
+
+}
+function mapAllItems() {
+    newPrices = menuOptions.map((product) => ({
+        ...product,
+        //name: product.name, 
+        price: product.price * 0.9,//vai dar 10% de desconto.
+        //vegan: product.vegan,
+        //src: product.src,
+    }))
+    //Spread Operator
+    showAll(newPrices)
+}
+function sumAllItems() {
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+
+    list.innerHTML = 
+    `<li>
+     <h2>Total da compra : ${formatCurrency(totalValue)}</h2>
+    </li>`
+}
+function filterAllItems() {
+    const filterJustVegan = menuOptions.filter( product => product.vegan === true);
+    console.log('Filter Vegan', filterJustVegan);
+    showAll(filterJustVegan)
+
+
+}
+
+
+
+
+buttonShowAll.addEventListener("click", () => showAll(menuOptions))
+buttonMapAll.addEventListener('click', mapAllItems)
+sumAll.addEventListener('click', sumAllItems)
+filterAll.addEventListener('click', filterAllItems)
---
+++ products.js
@@ -0,0 +1,8 @@
+const menuOptions = [
+    { name: 'X-Salada', price : 30.00, vegan: false, src: "img/xsalada.jpeg" },
+    { name: 'Bacon & Egg', price: 39, vegan: false, src: "img/bacon-egg.png" },
+    { name: 'Monstrúso Vegano', price: 55, vegan: true, src: "img/monstruoso-vegan.png" },
+    { name: 'Monstruoso', price: 50, vegan: false, src: "img/monstruoso.png" },
+    { name: 'X-bacon', price: 34, vegan: false, src: "img/xbacon.png" },
+    { name: 'X-Vegan', price: 45, vegan: true, src: "img/xvegan.png" },
+]
